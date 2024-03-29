06cfce042749f1615611e3c1842cae4ad3134fa4
Andrew Santos Maia <andrew.maia@alperseguros.com.br>
2024-03-29 19:23:14 -0300

    Ajustes

--- MediatR/NotificationsHandlers/ClienteInsertNotificationHandler.cs
+++ MediatR/NotificationsHandlers/ClienteInsertNotificationHandler.cs
@@ -3,13 +3,24 @@ using MediatR;
 
 namespace CSharp_MediatR.MediatR.NotificationsHandlers
 {
-    public class ClienteInsertNotificationHandler : INotificationHandler<ClienteInsertNotification>
+    public class ClienteInsertNotificationHandler1 : INotificationHandler<ClienteInsertNotification>
     {
         public Task Handle(ClienteInsertNotification notification, CancellationToken cancellationToken)
         {
             return Task.Run(() =>
             {
-                Console.WriteLine($"Eu sou o ClienteInsertNotificationHandler e recebi uma notificação de um novo cliente com o id {notification.ClienteId}!");
+                Console.WriteLine($"Eu sou o ClienteInsertNotificationHandler1 e recebi uma notificação de um novo cliente com o id {notification.ClienteId}!");
+            }, cancellationToken);
+        }
+    }
+
+    public class ClienteInsertNotificationHandler2 : INotificationHandler<ClienteInsertNotification>
+    {
+        public Task Handle(ClienteInsertNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"Eu sou o ClienteInsertNotificationHandler2 e recebi uma notificação de um novo cliente com o id {notification.ClienteId}!");
             }, cancellationToken);
         }
     }
